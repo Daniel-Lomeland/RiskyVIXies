cb3caa348d3df6c02049408ac7f746f8d51d6ece
Daniel Lomeland <dlomeland@gmail.com>
2019-04-27 10:31:27 -0500

    Add app routes

--- project_files/app.py
+++ project_files/app.py
@@ -23,23 +23,55 @@ def index():
     """Return the homepage."""
     return render_template("index.html")
 
+
+@app.route("/methodology.html")
+def methodology():
+    """Return the homepage."""
+    return render_template("methodology.html")
+
+@app.route("/regression.html")
+def regression():
+    """Return the homepage."""
+    return render_template("regression.html")
+
+@app.route("/plots.html")
+def plots():
+    """Return the homepage."""
+    return render_template("plots.html")
+
+@app.route("/business_news.html")
+def b_news():
+    """Return the homepage."""
+    return render_template("business_news.html")
+
+@app.route("/entrepreneurial_news.html")
+def e_news():
+    """Return the homepage."""
+    return render_template("entrepreneurial_news.html")
+
+@app.route("/financial_news.html")
+def f_news():
+    """Return the homepage."""
+    return render_template("financial_news.html")
+
+
 @app.route('/api',methods=['GET'])
 def predict():
     # Get latest news
     # nyt_news.getNews()
 
     # # Run sentiment analysis on latest news
-    # new_data = ask_google.get_sentiment()
+    new_data = ask_google.get_sentiment()
     # print(new_data)
-    words= word_2_vec.WTV()
-    print(words)
+    # words= word_2_vec.WTV()
+    # print(words)
     # Predict new VIX value
     #new_data = [7.000000e-01, 7.000000e-01, 6.000000e-01, 7.000000e-01, 6.000000e-01, 0.173205,4.000000e-01,0.550000,0.700000,0.700000]
-    # prediction = (model.predict(new_data))
-    # prediction_list = np.array(prediction).tolist()
-    # prediction_value = round(prediction_list[0][0],2)
-    # print(prediction_value)
+    prediction = (model.predict(new_data))
+    prediction_list = np.array(prediction).tolist()
+    prediction_value = round(prediction_list[0][0],2)
+    print(prediction_value)
     # return jsonify(prediction_value)
-    return jsonify(1)
+    return jsonify(3)
 if __name__ == "__main__":
     app.run(port=9999, debug=True)
